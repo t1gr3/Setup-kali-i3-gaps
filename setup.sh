3010397f9f37de30467ddf16a63406f0f5b16d93
t1gr3 <wamg-85@hotmail.com>
2022-01-25 14:41:29 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,17 +3,17 @@
 sudo apt update
 sudo apt install i3-gaps dolphin terminator rofi feh flameshot compton python3-psutil -y
 sudo unzip $PWD/src/fonts.zip
-mv $PWD/src/*.ttf /usr/share/fonts/truetype/hack
-mkdir ~/.config/i3
-mkdir ~/.config/i3status
-mkdir ~/.config/compton
-mkdir ~/.config/rofi
-mkdir ~/.config/wallpaper
-cp $PWD/src/i3/* ~/.config/i3
-cp $PWD/src/i3status/* ~/.config/i3status
-cp $PWD/src/compton/* ~/.config/compton
-cp $PWD/src/rofi/* ~/.config/rofi
-cp $PWD/src/wallpaper/* ~/.config/wallpaper
+sudo mv $PWD/*.ttf /usr/share/fonts/truetype/hack
+sudo mkdir ~/.config/i3
+sudo mkdir ~/.config/i3status
+sudo mkdir ~/.config/compton
+sudo mkdir ~/.config/rofi
+sudo mkdir ~/.config/wallpaper
+sudo cp $PWD/src/i3/* ~/.config/i3
+sudo cp $PWD/src/i3status/* ~/.config/i3status
+sudo cp $PWD/src/compton/* ~/.config/compton
+sudo cp $PWD/src/rofi/* ~/.config/rofi
+sudo cp $PWD/src/wallpaper/* ~/.config/wallpaper
 
 sudo apt autoremove -y
 sudo reboot
